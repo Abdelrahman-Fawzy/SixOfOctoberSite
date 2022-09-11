913154009e30eabc7011ef34fb6571d534211f80
Abdelrahman-Fawzy <Abdelrahmanawzy98@gmail.com>
2022-09-11 11:42:40 +0200

    Add First Version and First Structure

---
+++ dist/js/main.js
@@ -0,0 +1,103 @@
+$(function () {
+  $('.slider-container').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    rtl: true,
+    dots: true,
+  });
+  var videos_watched = 0;
+
+  $(".video_link").on("click", function() {
+    if (!($(this).hasClass("viewed"))) {
+      $(this).addClass("viewed");
+      videos_watched = videos_watched + 1;
+      $("#progress_sprite").append("<div></div>");
+      //This was added to show progress on fiddle. Not really 
+      //needed for progress bar.
+    }
+  });
+});
+
+
+// tabs function
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openTerm(evt, cityName) {
+  var i, tabcontent, tabTermsLinks;
+  tabcontent = document.getElementsByClassName("tabTermContent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tabTermsLinks = document.getElementsByClassName("tabTermsLinks");
+  for (i = 0; i < tabTermsLinks.length; i++) {
+    tabTermsLinks[i].className = tabTermsLinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpens").click();
+console.log(document.getElementById("defaultOpens"));
+document.getElementById("defaultTermOpen").click();
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) {
+      w3AddClass(x[i], "show");
+    }
+    x[i].parentElement.style.display = x[i].classList.contains('show') ? 'block' : 'none'
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    // btns[i].className.replace(' active', '')
+    console.log(this);
+    var current = document.getElementsByClassName("activeCourse");
+    console.log(current[0].className)
+    current[0].className = current[0].className.replace(" activeCourse", "");
+    this.className += " activeCourse";
+  });
+}
